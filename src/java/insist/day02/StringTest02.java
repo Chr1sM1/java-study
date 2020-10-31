2dd48c61b77bd0e0849335cadd5c02d1ff85b2bb
Chr1sM1 <mzs0119@qq.com>
2020-10-31 12:32:30 +0800

    day02

--- src/java/insist/day02/StringTest02.java
+++ src/java/insist/day02/StringTest02.java
@@ -1,8 +1,26 @@
-package insist.day02;/**
-  *@ClassName StringTest02
-  *@Description TODO
-  *@Author Chris
-  *@Date 2020/10/31
-**/
+package insist.day02;
+
+/**
+ * @ClassName StringTest02
+ * @Description 字符串测试
+ * @Author Chris
+ * @Date 2020/10/31
+ **/
 public class StringTest02 {
+    public static void main(String[] args) {
+        String s1 ="abc";
+        String s2 = new String("abc");
+        byte[] bytes ={97,98,99,100};
+        String s3 = new String(bytes);
+        //String已经重写了Object中的toString方法
+        System.out.println(s3);
+        //1代表起始下标，2代表长度
+        String s4 = new String(bytes,1,2);
+        System.out.println(s4);
+        char[] c1 = {'我','是','中','国','人'};
+        String s5 = new String(c1);
+        System.out.println(s5);
+        String s6 = new String(c1,2,2);
+        System.out.println(s6);
+    }
 }
