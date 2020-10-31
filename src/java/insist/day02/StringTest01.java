39d684b5ec04e220f320225c82d4d786df0f61b2
Chr1sM1 <mzs0119@qq.com>
2020-10-31 12:25:10 +0800

    day01

---
+++ src/java/insist/day02/StringTest01.java
@@ -0,0 +1,8 @@
+package insist.day02;/**
+  *@ClassName StringTest01
+  *@Description TODO
+  *@Author Chris
+  *@Date 2020/10/31
+**/
+public class StringTest01 {
+}
---
+++ src/java/insist/day02/StringTest02.java
@@ -0,0 +1,8 @@
+package insist.day02;/**
+  *@ClassName StringTest02
+  *@Description TODO
+  *@Author Chris
+  *@Date 2020/10/31
+**/
+public class StringTest02 {
+}
